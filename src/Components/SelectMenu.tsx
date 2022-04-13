2a2e9c749da5ccd492f4857e529436ed32b01a9f
HANGYESUK <akakakak66@naver.com>
2022-04-13 22:20:30 +0900

    feat: SelectMenu 제작

--- src/Components/SelectMenu.tsx
+++ src/Components/SelectMenu.tsx
@@ -8,17 +8,25 @@ function SelectMenu() {
             <h1>들어온 요청</h1>
             <p>파트너님에게 딱 맞는 요청서를 찾아보세요.</p>
         </div>
-        <div className='low'>
+        <div className='selection low'>
             <div>
                 <select name='가공방식'>
-                    <option></option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
                 </select>
                 <select name='재료'>
-                    <option></option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
                 </select>
             </div>
 
-            <div>
+            <div className='low'>
                 <button>버튼</button>
                 <p>상당중인 요청건만 보기</p>
             </div>
